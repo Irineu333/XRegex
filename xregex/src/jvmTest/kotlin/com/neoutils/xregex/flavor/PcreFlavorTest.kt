52e20e02e239c09e45c1643668198f919503c3db
Irineu A. Silva <mmmirineusilva@gmail.com>
2024-12-07 19:45:44 -0300

    test(jvm, pcre): subroutine reference

---
+++ xregex/src/jvmTest/kotlin/com/neoutils/xregex/flavor/PcreFlavorTest.kt
@@ -0,0 +1,51 @@
+package com.neoutils.xregex.flavor
+
+import com.neoutils.xregex.XMatch
+import com.neoutils.xregex.XRegex
+import com.neoutils.xregex.extension.findAll
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class PcreFlavorTest {
+
+    @Test
+    fun subroutineReference() {
+
+        val expected = listOf(
+            XMatch(
+                index = 0,
+                text = "((XRegex))",
+                range = 0..9,
+                groups = listOf(
+                    XMatch.Group(
+                        index = 0,
+                        text = "((XRegex))",
+                        range = 0..9
+                    ),
+                    XMatch.Group(
+                        index = 1,
+                        text = "((XRegex))",
+                        range = 0..9
+                    ),
+                    XMatch.Group(
+                        index = 2,
+                        text = "(XRegex)",
+                        range = 1..8
+                    ),
+                )
+            )
+        )
+
+        val actual = XRegex(
+            pattern = "(\\((\\w+|(?1))*\\))",
+            flags = listOf(XRegex.Flag.PCRE)
+        ).findAll(
+            text = "((XRegex))"
+        )
+
+        assertEquals(
+            expected,
+            actual
+        )
+    }
+}
